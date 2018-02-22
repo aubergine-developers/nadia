6a56becfa95af9e96dc081c487145887e31f90d7
Konrad Jałowiecki <dexter2206@gmail.com>
2018-02-22 03:52:12 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,39 @@
+"""Nadia: automatic generation of Marshmallow Schemas for OpenAPI 3 schemas."""
+
+from setuptools import setup, find_packages
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Software Development']
+
+MAJOR = 0
+MINOR = 1
+MICRO = 0
+VERSION = '{}.{}.{}'.format(MAJOR, MINOR, MICRO)
+
+LONG_DESCRIPTION = """Nadia is an automatic generator of marshmallow Schemas for your OpenAPI 3 schemas.
+
+Currently Nadia supports schemas with the following types:
+
+- integer, number
+- string
+- array
+- object (including nested ones)
+
+The required and nullable settings are also supported."""
+
+setup(
+    name='nadia',
+    version=VERSION,
+    description=__doc__,
+    long_description=LONG_DESCRIPTION,
+    platforms = ["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    author='Konrad Jałowiecki',
+    author_email='dexter2206@gmail.com',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    keywords='openapi schema validation'
+)
