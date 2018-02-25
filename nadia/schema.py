fb0bbd3e81e15f9ebe830da55001463c48542e5e
Konrad Jałowiecki <dexter2206@gmail.com>
2018-02-25 01:50:30 +0100

    Add base NadiaSchema as base schema for all nadia-produced schemas

---
+++ nadia/schema.py
@@ -0,0 +1,17 @@
+"""Base schema used by Nadia."""
+from marshmallow import Schema, validates_schema, ValidationError
+
+
+class NadiaSchema(Schema):
+    """Base marshmallow schema used by Nadia."""
+
+    @validates_schema(pass_original=True)
+    def check_unknown_fields(self, data, original_data):
+        """Validator raising ValidationError if extra keys are provided.
+
+        This validator is taken from the the official recipe located at:
+        http://marshmallow.readthedocs.io/en/latest/extending.html#validating-original-input-data
+        """
+        unknown = set(original_data) - set(self.fields)
+        if unknown:
+            raise ValidationError('Unknown field', unknown)
